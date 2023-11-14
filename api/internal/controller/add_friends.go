0fac72ccac06380decf5145d97c940c7c2002ebd
anh.mai <trianh4695@gmail.com>
2023-11-14 09:52:16 +0700

    -Change the way initialize gorm

--- api/internal/controller/add_friends.go
+++ api/internal/controller/add_friends.go
@@ -2,6 +2,8 @@ package controller
 
 import (
 	"context"
+
+	"gorm.io/gorm"
 )
 
 func (i CTRLImplement) AddFriends(ctx context.Context, userEmails []string) error {
@@ -9,38 +11,30 @@ func (i CTRLImplement) AddFriends(ctx context.Context, userEmails []string) erro
 	if err != nil {
 		return ServerError
 	}
-	// Start Business Logic
-	// Check whether ot not both users exists in db with for loop
-	for _, userEmail := range userEmails {
-		checkEmailExist, err := i.repo.CheckUserByEmail(ctx, userEmail)
+	return tx.Transaction(func(*gorm.DB) error {
+		// Start Business Logic
+		// Check whether ot not both users exists in db with for loop
+		for _, userEmail := range userEmails {
+			checkEmailExist, err := i.repo.CheckUserByEmail(ctx, userEmail)
+			if err != nil {
+				return ServerError
+			}
+			if !checkEmailExist {
+				return UserNotFound
+			}
+		}
+		// Check if both user already friends in db
+		checkFriendshipExist, err := i.repo.CheckFriendship(ctx, userEmails)
 		if err != nil {
-			tx.Rollback()
 			return ServerError
 		}
-		if !checkEmailExist {
-			tx.Rollback()
-			return UserNotFound
+		if checkFriendshipExist {
+			return FriendshipExisted
 		}
-	}
-
-	// Check if both user already friends in db
-	checkFriendshipExist, err := i.repo.CheckFriendship(ctx, userEmails)
-	if err != nil {
-		tx.Rollback()
-		return ServerError
-	}
-	if checkFriendshipExist {
-		tx.Rollback()
-		return FriendshipExisted
-	}
-
-	// Add friendship to db
-	if err = i.repo.AddFriendship(ctx, userEmails); err != nil {
-		tx.Rollback()
-		return ServerError
-	}
-
-	// Commit to transaction
-	tx.Commit()
-	return nil
+		// Add friendship to db
+		if err = i.repo.AddFriendship(ctx, userEmails); err != nil {
+			return ServerError
+		}
+		return nil
+	})
 }
