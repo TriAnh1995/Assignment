eb8824c0524824dd42ba3dc3332a24b08716a793
anh.mai <trianh4695@gmail.com>
2023-11-16 09:56:56 +0700

    - Remove redundant method

---
+++ api/internal/controller/add_friends.go
@@ -0,0 +1,36 @@
+package controller
+
+import (
+	"context"
+
+	"gorm.io/gorm"
+)
+
+func (i CTRLImplement) AddFriend(ctx context.Context, userEmails []string) error {
+	// Begin Transaction
+	err := i.gormDB.Transaction(func(*gorm.DB) error {
+		// Business Logic...
+		for _, userEmail := range userEmails {
+			checkEmailExist, err := i.repo.CheckUserByEmail(ctx, userEmail)
+			if err != nil {
+				return ServerError
+			}
+			if !checkEmailExist {
+				return UserNotFound
+			}
+		}
+
+		checkFriendshipExist, err := i.repo.CheckFriendship(ctx, userEmails)
+		if err != nil {
+			return ServerError
+		}
+		if checkFriendshipExist {
+			return FriendshipExisted
+		}
+		if err = i.repo.AddFriendship(ctx, userEmails); err != nil {
+			return ServerError
+		}
+		return nil
+	})
+	return err
+}
