dd661d7c10abf92ffd111bd5586a0670a229881b
anh.mai <trianh4695@gmail.com>
2023-11-20 17:05:15 +0700

    -FriendsList Handler

---
+++ api/internal/controller/friendslist_test.go
@@ -0,0 +1,90 @@
+package controller
+
+import (
+	"assignment/internal/model"
+	"assignment/internal/repository"
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestControllerImplement_FriendsList(t *testing.T) {
+	type expectedCheckUserByEmail struct {
+		expectedExist bool
+		expectedErr   error
+	}
+	type expectedGetFriendsList struct {
+		expectedList []string
+		expectedErr  error
+	}
+	testCases := []struct {
+		Name       string
+		InputEmail string
+		expectedCheckUserByEmail
+		expectedGetFriendsList
+		expectedResult model.FriendshipInfo
+		expectedErr    error
+	}{
+		{
+			Name:                     "Success",
+			InputEmail:               "user@example.com",
+			expectedCheckUserByEmail: expectedCheckUserByEmail{true, nil},
+			expectedGetFriendsList:   expectedGetFriendsList{[]string{"friend1", "friend2", "friend3", "friend4", "friend5"}, nil},
+			expectedResult: model.FriendshipInfo{
+				Amounts: 5,
+				List:    []string{"friend1", "friend2", "friend3", "friend4", "friend5"},
+			},
+			expectedErr: nil,
+		},
+		{
+			Name:                     "User Email not found",
+			InputEmail:               "user@example.com",
+			expectedCheckUserByEmail: expectedCheckUserByEmail{false, nil},
+			expectedGetFriendsList:   expectedGetFriendsList{nil, nil},
+			expectedResult:           model.FriendshipInfo{},
+			expectedErr:              UserNotFound,
+		},
+		{
+			Name:                     "Server error from CheckUserByEmail",
+			InputEmail:               "user@example.com",
+			expectedCheckUserByEmail: expectedCheckUserByEmail{true, ServerError},
+			expectedGetFriendsList:   expectedGetFriendsList{nil, nil},
+			expectedResult:           model.FriendshipInfo{},
+			expectedErr:              ServerError,
+		},
+		{
+			Name:                     "Server error from GetFriendsList",
+			InputEmail:               "user@example.com",
+			expectedCheckUserByEmail: expectedCheckUserByEmail{true, nil},
+			expectedGetFriendsList:   expectedGetFriendsList{nil, ServerError},
+			expectedResult:           model.FriendshipInfo{},
+			expectedErr:              ServerError,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.Name, func(t *testing.T) {
+
+			// Setup Instance
+			repo := &repository.MockRepository{}
+			ctrl := New(repo)
+			ctx := context.Background()
+
+			repo.On("CheckUserByEmail", ctx, tc.InputEmail).
+				Return(tc.expectedCheckUserByEmail.expectedExist, tc.expectedCheckUserByEmail.expectedErr)
+
+			repo.On("GetFriendsList", ctx, tc.InputEmail).
+				Return(tc.expectedGetFriendsList.expectedList, tc.expectedGetFriendsList.expectedErr)
+
+			result, err := ctrl.FriendsList(ctx, tc.InputEmail)
+
+			if err != nil {
+				require.EqualError(t, err, tc.expectedErr.Error())
+			} else {
+				require.NoError(t, tc.expectedErr)
+				require.Equal(t, tc.expectedResult, result)
+			}
+		})
+	}
+}
---
+++ api/internal/repository/get_friends_list_test.go
@@ -0,0 +1,57 @@
+package repository
+
+import (
+	"assignment/internal/repository/testdata"
+	"context"
+	"database/sql"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRepoImplement_GetFriendsList(t *testing.T) {
+	testCases := []struct {
+		Name           string
+		UserEmail      string
+		ExpectedResult []string
+		ExpectedErr    error
+		WantDBFail     bool
+	}{
+		{
+			Name:           "Success",
+			UserEmail:      "already_exist_email_1@example.com",
+			ExpectedResult: []string{"already_exist_email_2@example.com", "already_exist_email_3@example.com"},
+			ExpectedErr:    nil,
+			WantDBFail:     false,
+		},
+		{
+			Name:           "Internal Server Error",
+			UserEmail:      "already_exist_email_1@example.com",
+			ExpectedResult: nil,
+			ExpectedErr:    InternalErrorGetFriendList,
+			WantDBFail:     true,
+		},
+	}
+	ctx := context.Background()
+	for _, tc := range testCases {
+		t.Run(tc.Name, func(t *testing.T) {
+			testdata.TestDatabase(t, func(tx *sql.Tx) {
+				testdata.LoadTestSQLFile(t, tx, "testdata/testdata_for_friendship.sql")
+				repo := New(tx)
+				if tc.WantDBFail {
+					dbMock, _, _ := sqlmock.New()
+					repo = New(dbMock)
+				}
+
+				listOfFriends, err := repo.GetFriendsList(ctx, tc.UserEmail)
+
+				if err == nil {
+					require.Equal(t, listOfFriends, tc.ExpectedResult)
+				} else {
+					require.EqualError(t, err, tc.ExpectedErr.Error())
+				}
+			})
+		})
+	}
+}
