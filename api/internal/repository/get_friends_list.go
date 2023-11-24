951fc9c69661954ca6bcfe4b5c1b063470add025
anh.mai <trianh4695@gmail.com>
2023-11-24 09:10:14 +0700

    -FriendsList Controller

--- api/internal/repository/get_friends_list.go
+++ api/internal/repository/get_friends_list.go
@@ -3,30 +3,27 @@ package repository
 import (
 	"assignment/internal/repository/orm"
 	"context"
-
-	"github.com/volatiletech/sqlboiler/v4/queries/qm"
 )
 
 // GetFriendsList GetFriendList retrieve a list of friend names a user have
 func (i RepoImplement) GetFriendsList(ctx context.Context, userEmail string) ([]string, error) {
-	// Using method `All` from SQLBoiler to extract all the rows in 'friendship' table that contain userEmail
-	friends, err := orm.Friendships(
-		qm.Where("user_email_1 = ? OR user_email_2 = ?", userEmail, userEmail)).
-		All(ctx, i.pgConn)
+	var listOfFriends []string
+	friends1, err := orm.Friendships(
+		orm.FriendshipWhere.UserEmail1.EQ(userEmail)).All(ctx, i.pgConn)
 	if err != nil {
 		return []string{}, err
 	}
+	for _, friend := range friends1 {
+		listOfFriends = append(listOfFriends, friend.UserEmail2)
+	}
 
-	// Create a slice to store list of friends
-	listOfFriends := make([]string, len(friends))
-
-	// Put all the rows in 'for' loop, check for each column which have emails that not userEmail and store them in the list
-	for n, friend := range friends {
-		if friend.UserEmail1 != userEmail {
-			listOfFriends[n] = friend.UserEmail1
-		} else {
-			listOfFriends[n] = friend.UserEmail2
-		}
+	friends2, err := orm.Friendships(
+		orm.FriendshipWhere.UserEmail2.EQ(userEmail)).All(ctx, i.pgConn)
+	if err != nil {
+		return []string{}, err
+	}
+	for _, friend := range friends2 {
+		listOfFriends = append(listOfFriends, friend.UserEmail1)
 	}
 	return listOfFriends, err
 }
