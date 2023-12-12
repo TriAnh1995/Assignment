443ea8a9ef7e74da1d79ef114b069032576fe9a1
anh.mai <trianh4695@gmail.com>
2023-12-12 17:26:45 +0700

    -  Initializing AddSubscription Controller

--- api/internal/repository/mock_Repository.go
+++ api/internal/repository/mock_Repository.go
@@ -66,6 +66,54 @@ func (_m *MockRepository) CheckFriendship(_a0 context.Context, _a1 []string) (bo
 	return r0, r1
 }
 
+// CheckIfBlocked provides a mock function with given fields: _a0, _a1
+func (_m *MockRepository) CheckIfBlocked(_a0 context.Context, _a1 []string) (bool, error) {
+	ret := _m.Called(_a0, _a1)
+
+	var r0 bool
+	var r1 error
+	if rf, ok := ret.Get(0).(func(context.Context, []string) (bool, error)); ok {
+		return rf(_a0, _a1)
+	}
+	if rf, ok := ret.Get(0).(func(context.Context, []string) bool); ok {
+		r0 = rf(_a0, _a1)
+	} else {
+		r0 = ret.Get(0).(bool)
+	}
+
+	if rf, ok := ret.Get(1).(func(context.Context, []string) error); ok {
+		r1 = rf(_a0, _a1)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// CheckIfFollowed provides a mock function with given fields: _a0, _a1
+func (_m *MockRepository) CheckIfFollowed(_a0 context.Context, _a1 []string) (bool, error) {
+	ret := _m.Called(_a0, _a1)
+
+	var r0 bool
+	var r1 error
+	if rf, ok := ret.Get(0).(func(context.Context, []string) (bool, error)); ok {
+		return rf(_a0, _a1)
+	}
+	if rf, ok := ret.Get(0).(func(context.Context, []string) bool); ok {
+		r0 = rf(_a0, _a1)
+	} else {
+		r0 = ret.Get(0).(bool)
+	}
+
+	if rf, ok := ret.Get(1).(func(context.Context, []string) error); ok {
+		r1 = rf(_a0, _a1)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // CheckUserByEmail provides a mock function with given fields: _a0, _a1
 func (_m *MockRepository) CheckUserByEmail(_a0 context.Context, _a1 string) (bool, error) {
 	ret := _m.Called(_a0, _a1)
@@ -88,6 +136,34 @@ func (_m *MockRepository) CheckUserByEmail(_a0 context.Context, _a1 string) (boo
 	}
 
 	return r0, r1
+}
+
+// SubscribeToBlocked provides a mock function with given fields: _a0, _a1
+func (_m *MockRepository) SubscribeToBlocked(_a0 context.Context, _a1 []string) error {
+	ret := _m.Called(_a0, _a1)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(context.Context, []string) error); ok {
+		r0 = rf(_a0, _a1)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
+// SubscribeToNonBlocked provides a mock function with given fields: _a0, _a1
+func (_m *MockRepository) SubscribeToNonBlocked(_a0 context.Context, _a1 []string) error {
+	ret := _m.Called(_a0, _a1)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(context.Context, []string) error); ok {
+		r0 = rf(_a0, _a1)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
 }
 
 type mockConstructorTestingTNewMockRepository interface {
