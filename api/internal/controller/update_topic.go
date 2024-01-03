57620214587ff052db7e5591840857c3c7ca32d5
anh.mai <trianh4695@gmail.com>
2024-01-03 13:18:10 +0700

    - Adding Local Function for Optimization

--- api/internal/controller/update_topic.go
+++ api/internal/controller/update_topic.go
@@ -20,48 +20,31 @@ func (i CTRLImplement) UpdateTopic(ctx context.Context, updateInfo model.UpdateI
 		return []string{}, ServerError
 	}
 
-	// Extract list of blocked users and list of friends that the Sender have
+	// Extract list of blocked users, followed user and friends of the Sender
 	blockedList, err := i.repo.GetBlockedList(ctx, updateInfo.Sender)
 	if err != nil {
 		return []string{}, ServerError
 	}
-
 	friendList, err := i.repo.GetFriendsList(ctx, updateInfo.Sender)
 	if err != nil {
 		return []string{}, ServerError
 	}
-
-	// Using map string to find the list of friends that didn't get blocked
-	var nonBlockedFriends []string
-	Map := make(map[string]bool)
-	for _, username := range friendList {
-		Map[username] = true
-	}
-	for _, friend := range blockedList {
-		if !Map[friend] {
-			nonBlockedFriends = append(nonBlockedFriends, friend)
-		}
-	}
-
-	// Extract the list of follower from subscription db
 	followedList, err := i.repo.GetFollowerList(ctx, updateInfo.Sender)
 	if err != nil {
 		return []string{}, ServerError
 	}
 
-	// Create a unique map to store elements from the nonBlockedFriends and followedList
-	uniqueMap := make(map[string]struct{})
-	for _, s := range nonBlockedFriends {
-		uniqueMap[s] = struct{}{}
-	}
-	for _, s := range followedList {
-		uniqueMap[s] = struct{}{}
-	}
-	// Create a new slice to hold the unique emails from nonBlockedFriends and followedList
-	updateReceivedList := make([]string, 0, len(uniqueMap))
-	for s := range uniqueMap {
-		updateReceivedList = append(updateReceivedList, s)
-	}
+	// Among three lists that got extract, we need the list of friends emails that didn't get blocked
+	blockedFriendList := ExtractCommonElements(blockedList, friendList)
+	nonBlockedFriendList := RemoveElements(friendList, blockedFriendList)
 
-	return updateReceivedList, nil
+	// And a unique emails in follower and friends circle
+	commonInFollowersAndFriends := ExtractCommonElements(followedList, friendList)
+	combinedFriendsAndFollower := append(nonBlockedFriendList, followedList...)
+	uniqueFollowerAndFriends := RemoveElements(combinedFriendsAndFollower, commonInFollowersAndFriends)
+
+	// Combine with the emails mentioned in the text, we got the final list
+	finalList := append(uniqueFollowerAndFriends, updateInfo.MentionedEmail)
+
+	return finalList, nil
 }
