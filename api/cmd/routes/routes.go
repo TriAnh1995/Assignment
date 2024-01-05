958cbe46c09ef270ed9a386e3a591da4a5253005
anh.mai <trianh4695@gmail.com>
2024-01-05 09:33:18 +0700

    - Resolve Conflict During Rebase

--- api/cmd/routes/routes.go
+++ api/cmd/routes/routes.go
@@ -12,9 +12,9 @@ type Route struct {
 
 func (r Route) Routes(rtr *gin.Engine) {
 	rtr.POST("/users", r.h.AddUsers())
-	rtr.POST("/friends", r.h.AddFriend())
 	rtr.POST("/subscriptions", r.h.AddSubscription())
-  
-	rtr.GET("/friends/list", r.h.FriendsList()) // URL example: 'localhost:3000/friends/list?email=user1@example.com'
+
+	rtr.POST("/friends", r.h.AddFriend())
+	rtr.GET("/friends/list", r.h.FriendsList())     // URL example: 'localhost:3000/friends/list?email=user1@example.com'
 	rtr.GET("/friends/common", r.h.CommonFriends()) // URL example: 'localhost:3000/friends/common?email1=user1@example.com&email2=user2@example.com'
 }
