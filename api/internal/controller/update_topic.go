47bbe9fe6b5536c17437cbd6441b00fa448cc5fa
anh.mai <trianh4695@gmail.com>
2024-01-17 08:34:33 +0700

    - a

--- api/internal/controller/update_topic.go
+++ api/internal/controller/update_topic.go
@@ -15,6 +15,7 @@ func (i CTRLImplement) UpdateTopic(ctx context.Context, updateInfo model.UpdateI
 	if err := i.repo.UpdateTopic(ctx, updateInfo); err != nil {
 		return []string{}, middleware.ServerError
 	}
+
 	receivedUpdateList, err := i.repo.GetReceivedUpdateList(ctx, updateInfo.Sender)
 	if err != nil {
 		return []string{}, middleware.ServerError
@@ -22,25 +23,4 @@ func (i CTRLImplement) UpdateTopic(ctx context.Context, updateInfo model.UpdateI
 	finalList := append(receivedUpdateList, updateInfo.MentionedEmails...)
 
 	return finalList, err
-
-	//blockedList, err := i.repo.GetBlockedList(ctx, updateInfo.Sender)
-	//if err != nil {
-	//	return []string{}, ServerError
-	//}
-	//friendList, err := i.repo.GetFriendsList(ctx, updateInfo.Sender)
-	//if err != nil {
-	//	return []string{}, ServerError
-	//}
-
-	//
-	//// Among three lists that got extract, we need the list of friends emails that didn't get blocked
-	//blockedFriendList := ExtractCommonElements(blockedList, friendList)
-	//nonBlockedFriendList := RemoveElements(friendList, blockedFriendList)
-	//
-	//// And a unique emails in follower and friends circle
-	//uniqueFollowerAndFriends := CombineUnique(followedList, nonBlockedFriendList)
-	//
-	//// Combine with the emails mentioned in the text, we got the final list
-	//finalList := append(uniqueFollowerAndFriends, updateInfo.MentionedEmails...)
-
 }
